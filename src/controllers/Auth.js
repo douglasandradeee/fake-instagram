7fa36f9a504aeae7dfb9236784df72a764a2d530
Douglas Andrade <dba12@hotmail.com>
2021-09-28 20:50:40 -0300

    Authentication
    fazendo autenticação do usuário

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -7,9 +7,8 @@ const authController = {
     return res.render("auth/register");
   },
   async registerUser(req, res) {
-    // console.log(req.body)
-    const {email, name, username, password} = req.body;
     try {
+      const {email, name, username, password} = req.body;
       const user = await User.create({
         name,
         email,
@@ -18,11 +17,31 @@ const authController = {
         avatar:"link",
         create_at: new Date().toISOString(),
       })
-
       return res.redirect("/login");
     }  catch(err){
       console.log(err);
       return res.redirect("/registro")
+    }
+  },
+  async login(req, res) {
+    try {
+      const {email, password} = req.body;
+      const user = await User.findOne({
+        where:{
+          email
+        }
+      });
+      if(!user){
+        return res.render("auth/login", {error: "Usuário não existe!"}) 
+      }
+    
+      if(user.password != password) {
+        return res.render("auth/login", {error: "A senha está errada!"}) 
+      }
+      return res.redirect("/home");
+    } catch(err) {
+      console.log(err);
+      return res.render("auth/login", {error: "Sistema indisponível tente novamente mais tarde!"})
     }
   },
 };
